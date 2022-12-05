69f969c381574fe76efac51fe316bb3a2ba8e486
Sabrina <59457991+syenko@users.noreply.github.com>
2022-12-04 21:42:18 -0800

    Add Day 5 python solutions

---
+++ pythonAOC/archive/5part1.py
@@ -0,0 +1,36 @@
+f = open("../inputs/5.txt", "r")
+
+crates = []
+
+for x in f:
+    if x == "\n":
+        break
+    if x[1] == "1":
+        continue
+    print(x)
+    crates.append(x)
+
+stacks = []
+
+for i in range(9):
+    stacks.append([])
+
+for i in range(len(crates)-1,-1, -1):
+    for j in range(1,len(crates[i]),4):
+        if (crates[i][j] != ' '):
+            stacks[(j-1)//4].append(crates[i][j])
+
+for x in f:
+    vals = x.split(" ")
+    numMove = int(vals[1])
+    start = int(vals[3]) - 1
+    end = int(vals[5]) - 1
+
+    for i in range(numMove):
+        stacks[end].append(stacks[start].pop())
+
+
+# print(stacks)
+for i in stacks:
+    print(i.pop(),end="")
+
