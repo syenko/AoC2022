676940519c03e3da7f5bf73b3084dd25b71eebfa
Sabrina <59457991+syenko@users.noreply.github.com>
2022-12-06 21:30:52 -0800

    Add Day 7 Code

---
+++ pythonAOC/archive/7part1.py
@@ -0,0 +1,46 @@
+f = open("../inputs/7.txt", "r")
+
+lines = [x for x in f]
+
+directories = {}
+
+cwd = ""
+
+for i in range(len(lines)):
+    x = lines[i].strip()
+    # cd command
+    if x[0] == "$":
+        print(f"cwd: {cwd}")
+        print(f"x: {x}")
+        chars = x.split(" ")
+
+        if chars[1] == 'cd':
+            if chars[2] == "..":
+                cwd = ''.join(folder + ";" for folder in cwd.split(";")[:-2])
+                continue
+            else:
+                cwd += chars[2] + ";" # add file path to end of current working directory
+                i += 2
+                if (cwd not in directories):
+                    directories[cwd] = 0
+
+                while i < len(lines) and lines[i][0] != "$":
+                    if lines[i].split(" ")[0] == "dir":
+                        i += 1
+                        continue
+                    else:
+                        folders = cwd.split(";")[:-1]
+                        build_dir = ""
+                        for folder in folders:
+                            build_dir += folder + ";"
+                            directories[build_dir] += int(lines[i].split(" ")[0])
+                    i += 1
+
+print(directories)
+
+sum = 0
+for (key, val) in directories.items():
+    if (val < 100000):
+        sum += val
+
+print(sum)
