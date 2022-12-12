1d9d9dc7e184b03403ccb0a437022b31be5fa29c
Sabrina <59457991+syenko@users.noreply.github.com>
2022-12-11 21:51:19 -0800

    Clean up Day 12 code

--- pythonAOC/archive/12part1.py
+++ pythonAOC/archive/12part1.py
@@ -1,4 +1,4 @@
-f = open("input.txt", "r")
+f = open("inputs/12.txt", "r")
 
 lines = [x.strip() for x in f]
 
@@ -13,6 +13,7 @@ visited = {}
 
 MAX_VAL = 10000000000
 
+# find start and end
 for i in range(len(chars)):
     for j in range(len(chars[i])):
         if chars[i][j] == "E":
@@ -22,50 +23,43 @@ for i in range(len(chars)):
             startPos = (i, j)
             chars[i][j] = "a"
 
+
 # going backwards checking
-def checkNearest(i, j):
+def initializeVertex(i, j):
     val = ord(chars[i][j])
 
-    graph[(i,j)] = []
+    graph[(i, j)] = []
     dist[(i, j)] = MAX_VAL
     visited[(i, j)] = False
 
-    if i > 0 and val - ord(chars[i-1][j]) <= 1:
-        graph[(i, j)].append((i-1, j))
+    if i > 0 and val - ord(chars[i - 1][j]) <= 1:
+        graph[(i, j)].append((i - 1, j))
 
-    if i < len(chars)-1 and val - ord(chars[i + 1][j]) <= 1:
+    if i < len(chars) - 1 and val - ord(chars[i + 1][j]) <= 1:
         graph[(i, j)].append((i + 1, j))
 
-    if j > 0 and val - ord(chars[i][j-1]) <= 1:
-        graph[(i, j)].append((i, j-1))
+    if j > 0 and val - ord(chars[i][j - 1]) <= 1:
+        graph[(i, j)].append((i, j - 1))
 
-    if j < len(chars[i])-1 and val - ord(chars[i][j+1]) <= 1:
-        graph[(i, j)].append((i, j+1))
+    if j < len(chars[i]) - 1 and val - ord(chars[i][j + 1]) <= 1:
+        graph[(i, j)].append((i, j + 1))
 
+
+# initialize all vertices
 for i in range(len(chars)):
     for j in range(len(chars[i])):
-        checkNearest(i, j)
+        initializeVertex(i, j)
 
-dist[endPos] =0
-
-sptSet = set()
-
+dist[endPos] = 0
 toSearch = [endPos]
-
-for (key, val) in graph.items():
-    print(f"{key} :: {val}")
-
 curDist = 0
+# bfs?
 while len(toSearch) != 0:
     top = toSearch.pop(0)
     for point in graph[top]:
-        # print(f"p {point} d {dist[top]}")
         dist[point] = min(dist[point], dist[top] + 1)
         if not visited[point]:
             visited[point] = True
             toSearch.append(point)
 
-print(startPos)
-
 print(dist[startPos])
-
--- pythonAOC/archive/12part2.py
+++ pythonAOC/archive/12part2.py
@@ -1,4 +1,4 @@
-f = open("input.txt", "r")
+f = open("inputs/12.txt", "r")
 
 lines = [x.strip() for x in f]
 
@@ -15,6 +15,7 @@ MAX_VAL = 10000000000
 
 check = []
 
+# find start, end, and all points with elevation a
 for i in range(len(chars)):
     for j in range(len(chars[i])):
         if chars[i][j] == "E":
@@ -24,57 +25,49 @@ for i in range(len(chars)):
             startPos = (i, j)
             chars[i][j] = "a"
 
-            check.append((i,j))
+            check.append((i, j))
+
 
 # going backwards checking
-def checkNearest(i, j):
+def initializeVertex(i, j):
     val = ord(chars[i][j])
 
-    graph[(i,j)] = []
+    graph[(i, j)] = []
     dist[(i, j)] = MAX_VAL
     visited[(i, j)] = False
 
-    if i > 0 and val - ord(chars[i-1][j]) <= 1:
-        graph[(i, j)].append((i-1, j))
+    if i > 0 and val - ord(chars[i - 1][j]) <= 1:
+        graph[(i, j)].append((i - 1, j))
 
-    if i < len(chars)-1 and val - ord(chars[i + 1][j]) <= 1:
+    if i < len(chars) - 1 and val - ord(chars[i + 1][j]) <= 1:
         graph[(i, j)].append((i + 1, j))
 
-    if j > 0 and val - ord(chars[i][j-1]) <= 1:
-        graph[(i, j)].append((i, j-1))
+    if j > 0 and val - ord(chars[i][j - 1]) <= 1:
+        graph[(i, j)].append((i, j - 1))
 
-    if j < len(chars[i])-1 and val - ord(chars[i][j+1]) <= 1:
-        graph[(i, j)].append((i, j+1))
+    if j < len(chars[i]) - 1 and val - ord(chars[i][j + 1]) <= 1:
+        graph[(i, j)].append((i, j + 1))
 
+
+# initialize all vertices
 for i in range(len(chars)):
     for j in range(len(chars[i])):
-        checkNearest(i, j)
+        initializeVertex(i, j)
 
-dist[endPos] =0
-
-sptSet = set()
-
+dist[endPos] = 0
 toSearch = [endPos]
-
-for (key, val) in graph.items():
-    print(f"{key} :: {val}")
-
 curDist = 0
+
+# bfs?
 while len(toSearch) != 0:
     top = toSearch.pop(0)
     for point in graph[top]:
-        # print(f"p {point} d {dist[top]}")
         dist[point] = min(dist[point], dist[top] + 1)
         if not visited[point]:
             visited[point] = True
             toSearch.append(point)
 
-print(startPos)
-
 ans = MAX_VAL
-
 for pos in check:
-    ans = min(dist[pos],ans)
-
+    ans = min(dist[pos], ans)
 print(ans)
-
